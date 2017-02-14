485dba18b9a30c2b7c1335d561f62620a1cda37a
jeisse <jeisserocha@gmail.com>
2017-02-14 22:33:43 +0000

    Included webhook on the correct place

--- app.js
+++ app.js
@@ -10,20 +10,3 @@ module.exports = require('./config/express')(app, config);
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
-
-
-/*
- * Use your own validation token. Check that the token used in the Webhook 
- * setup is the same token used here.
- *
- */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'hellopanpan') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
