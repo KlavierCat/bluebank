5519bf4eaf812dfa76ca1a13945a38968bda58d7
jeisse <jeisserocha@gmail.com>
2017-02-14 21:08:17 +0000

    implemening webhook for messenger api

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
+
+/*
+ * Use your own validation token. Check that the token used in the Webhook 
+ * setup is the same token used here.
+ *
+ */
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'hellopanpan') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
