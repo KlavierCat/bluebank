846bfebf2ac64bf96a2e6144b263c405e49dc678
KlavierCat <plin@tcd.ie>
2017-02-17 23:35:02 +0000

    add more users

--- config/users.js
+++ config/users.js
@@ -6,11 +6,19 @@ var users = {
     'savingAccountId':'58a6003a23ccb9d022065454',
     'savingAccountNumber':'50000389'
   },
-  000000000000000: {
+  1217825631647606: {
     'givenName':'Mauricio',
     'customerId':'58a1731206d0f9e7189d1db2',
     'currentAccountId':'58a1731206d0f9e7189d1db',
-    'savingAccountId':'58a1731206d0f9e7189d1db4'
+    'savingAccountId':'58a1731206d0f9e7189d1db4',
+    'savingAccountNumber':'50000375'
+  },
+  1278860642198812: {
+    'givenName':'Jeisse',
+    'customerId':'58a1737f06d0f9e7189d1db6',
+    'currentAccountId':'58a1737f06d0f9e7189d1db7',
+    'savingAccountId':'58a1737f06d0f9e7189d1db8',
+    'savingAccountNumber':'50000376'
   }
 };
 
