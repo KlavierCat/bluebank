0f67f97c182f1c82b194f92256157ed1ae5197f0
mauriciodeoliveirareis <mauriciodeoliveirareis@gmail.com>
2017-02-18 00:23:30 +0000

    Merge branch 'master' of https://github.com/Coding-Drunk/bluebank
    # Conflicts:
    #	app/views/index.jade

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -28,80 +28,80 @@ router.get('/', function (req, res, next) {
     });
 });
 
-/*
- * Use your own validation token. Check that the token used in the Webhook
- * setup is the same token used here.
- *
- */
-router.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'hellopanpan') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-//TODO give the option to send an account name, we gonna use probably only two accounts in the demo so, can be only a hardcoded if
-router.get('/account', function (req, res) {
-  getAccountForDefaultCostumer(function(data) {
-    console.log(data.accountBalance);
-    //discover why accountBalance is not sent back but the entire object is
-    res.send(data);
-  });
-
-});
-
-function getAccountForDefaultCostumer(callback) {
-
-  return https.get({
-    host: 'bluebank.azure-api.net',
-    path: '/api/v0.6.3/customers?',
-    headers: {"Ocp-Apim-Subscription-Key": devKey, "bearer": bearer, accept: '*/*'}
-  }, function(response) {
-    // Continuously update stream with data
-    var body = '';
-    response.on('data', function(d) {
-      body += d;
-    });
-    response.on('error', function(e) {
-      console.log("Got error: " + e.message);
-    });
-    response.on('end', function() {
-      console.log(body);
-      var costumer = JSON.parse(body)[0];
-      console.log(costumer.id);
-      getAccount(costumer.id, callback);
-    });
-  });
-
-};
-
-function getAccount(costumerId, callback) {
-
-  return https.get({
-    host: 'bluebank.azure-api.net',
-    path: '/api/v0.6.3/customers/' + costumerId +"/accounts?",
-    headers: {"Ocp-Apim-Subscription-Key": devKey, "bearer": bearer, accept: '*/*'}
-  }, function(response) {
-    var body = '';
-    response.on('data', function(d) {
-      body += d;
-    });
-    response.on('error', function(e) {
-      console.log("Got error: " + e.message);
-    });
-    response.on('end', function() {
-      //prints to guarantee that all the data was flushed
-      console.log(body);
-      var account = JSON.parse(body)[0];
-      console.log(account);
-      //getAccount(costumer.id, callback);
-
-      callback(account);
-    });
-  });
-
-};
+///*
+// * Use your own validation token. Check that the token used in the Webhook
+// * setup is the same token used here.
+// *
+// */
+//router.get('/webhook', function(req, res) {
+//  if (req.query['hub.mode'] === 'subscribe' &&
+//      req.query['hub.verify_token'] === 'hellopanpan') {
+//    console.log("Validating webhook");
+//    res.status(200).send(req.query['hub.challenge']);
+//  } else {
+//    console.error("Failed validation. Make sure the validation tokens match.");
+//    res.sendStatus(403);
+//  }
+//});
+//
+////TODO give the option to send an account name, we gonna use probably only two accounts in the demo so, can be only a hardcoded if
+//router.get('/account', function (req, res) {
+//  getAccountForDefaultCostumer(function(data) {
+//    console.log(data.accountBalance);
+//    //discover why accountBalance is not sent back but the entire object is
+//    res.send(data);
+//  });
+//
+//});
+//
+//function getAccountForDefaultCostumer(callback) {
+//
+//  return https.get({
+//    host: 'bluebank.azure-api.net',
+//    path: '/api/v0.6.3/customers?',
+//    headers: {"Ocp-Apim-Subscription-Key": devKey, "bearer": bearer, accept: '*/*'}
+//  }, function(response) {
+//    // Continuously update stream with data
+//    var body = '';
+//    response.on('data', function(d) {
+//      body += d;
+//    });
+//    response.on('error', function(e) {
+//      console.log("Got error: " + e.message);
+//    });
+//    response.on('end', function() {
+//      console.log(body);
+//      var costumer = JSON.parse(body)[0];
+//      console.log(costumer.id);
+//      getAccount(costumer.id, callback);
+//    });
+//  });
+//
+//};
+//
+//function getAccount(costumerId, callback) {
+//
+//  return https.get({
+//    host: 'bluebank.azure-api.net',
+//    path: '/api/v0.6.3/customers/' + costumerId +"/accounts?",
+//    headers: {"Ocp-Apim-Subscription-Key": devKey, "bearer": bearer, accept: '*/*'}
+//  }, function(response) {
+//    var body = '';
+//    response.on('data', function(d) {
+//      body += d;
+//    });
+//    response.on('error', function(e) {
+//      console.log("Got error: " + e.message);
+//    });
+//    response.on('end', function() {
+//      //prints to guarantee that all the data was flushed
+//      console.log(body);
+//      var account = JSON.parse(body)[0];
+//      console.log(account);
+//      //getAccount(costumer.id, callback);
+//
+//      callback(account);
+//    });
+//  });
+//
+//};
